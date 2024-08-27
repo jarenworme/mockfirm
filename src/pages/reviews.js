6d3f64c57631c3c9a7ef18eef1ff8b7d3243a624
jarenworme <jarenworme@gmail.com>
2024-08-27 14:12:25 -0400

    major styling in reviews completed without icons

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WEBADDRESS from "../webAddress";
+import '../styles/reviews.css';
+import '../styles/nav.css';
 
 export default function Reviews () {
     const navigate = useNavigate();
     
     const [reviewArray, setReviewArray] = useState([]);
-    const [currTab, setCurrTab] = useState('all');
+    const [currTab, setCurrTab] = useState(0);
 
     // grab all reviews from api
     useEffect(() => {
@@ -21,13 +23,9 @@ export default function Reviews () {
             });
     }, []);
 
-    useEffect(() => {
-        console.log(reviewArray);
-    }, [reviewArray]);
-
     const switchTab = (tab) => {
         if(tab == 0) {
-            setCurrTab('all');
+            setCurrTab(tab);
             var Addr = WEBADDRESS + 'api/reviews/';
             axios.get(Addr)
                 .then(response => {
@@ -37,8 +35,7 @@ export default function Reviews () {
                     console.log(error);
                 });
         } else {
-            let tabName = tab + '-star';
-            setCurrTab(tabName);
+            setCurrTab(tab);
             var Addr = WEBADDRESS + 'api/reviews/' + tab + '/';
             axios.get(Addr)
                 .then(response => {
@@ -48,31 +45,75 @@ export default function Reviews () {
                     console.log(error);
                 });
         }
-
     }
+
+    const navigateHome = () => navigate('/', {replace: false});
+    const navigatePublish = () => navigate('/publishreview', {replace: false});
 
 
     return (
-        <div>
-            <h1>see our reviews</h1>
-            <button onClick={() => switchTab(0)}>all</button>
-            <button onClick={() => switchTab(5)}>5-star</button>
-            <button onClick={() => switchTab(4)}>4-star</button>
-            <button onClick={() => switchTab(3)}>3-star</button>
-            <button onClick={() => switchTab(2)}>2-star</button>
-            <button onClick={() => switchTab(1)}>1-star</button>
-            <div>
-                {reviewArray.length == 0 ?
-                <div>
-                    <h2>currently no {currTab} reviews.</h2>
+        <div className="review-page-wrapper">
+            <div className="nav-bar">
+                <div className="nav-left">
+                    <h1 className="logo1">JW</h1>
+                    <h1 className="logo2">Electrical</h1>
                 </div>
-                : reviewArray.map((item, index) => (
-                    <div key={item.id}>
-                        <h1>{item.stars}</h1>
-                        <h1>{item.content}</h1>
-                        <h1>{item.reviewer}</h1>
+                <div className="nav-right">
+                    <button onClick={navigateHome} className="nav-home">home</button>
+                </div>
+            </div>
+            <div className="review-title-wrapper">
+                <h1 className="review-title">Our Reviews</h1>
+                <p className="review-subtitle">See what makes us special</p>
+            </div>
+            <div className="tabs-wrapper">
+                <div className="tab-wrapper">
+                    <button onClick={() => switchTab(0)} className={`review-tab ${currTab === 0 ? 'active' : ''}`}>all</button>
+                    <div className={`review-tab-bar ${currTab === 0 ? 'activeb' : ''}`}>.</div>
+                </div>
+                <div className="tab-wrapper">
+                    <button onClick={() => switchTab(5)} className={`review-tab ${currTab === 5 ? 'active' : ''}`}>5-star</button>
+                    <div className={`review-tab-bar ${currTab === 5 ? 'activeb' : ''}`}>.</div>
+                </div>
+                <div className="tab-wrapper">
+                    <button onClick={() => switchTab(4)} className={`review-tab ${currTab === 4 ? 'active' : ''}`}>4-star</button>
+                    <div className={`review-tab-bar ${currTab === 4 ? 'activeb' : ''}`}>.</div>
+                </div>
+                <div className="tab-wrapper">
+                    <button onClick={() => switchTab(3)} className={`review-tab ${currTab === 3 ? 'active' : ''}`}>3-star</button>
+                    <div className={`review-tab-bar ${currTab === 3 ? 'activeb' : ''}`}>.</div>
+                </div>
+                <div className="tab-wrapper">
+                    <button onClick={() => switchTab(2)} className={`review-tab ${currTab === 2 ? 'active' : ''}`}>2-star</button>
+                    <div className={`review-tab-bar ${currTab === 2 ? 'activeb' : ''}`}>.</div>
+                </div>
+                <div className="tab-wrapper">
+                    <button onClick={() => switchTab(1)} className={`review-tab ${currTab === 1 ? 'active' : ''}`}>1-star</button>
+                    <div className={`review-tab-bar ${currTab === 1 ? 'activeb' : ''}`}>.</div>
+                </div>
+            </div>
+            <div className="reviews-container-wrapper">
+                <div className="arrow-wrapper">l</div>
+                <div className="reviews-container">
+                    {reviewArray.length == 0 ?
+                    <div>
+                        <h2>currently no {currTab}-star reviews.</h2>
                     </div>
-                ))}
+                    : reviewArray.map((item, index) => (
+                        <div key={item.id} className="review-card">
+                            <h1>{item.stars}</h1>
+                            <p>{item.content}</p>
+                            <h4>{item.reviewer}</h4>
+                        </div>
+                    ))}
+                </div>
+                <div className="arrow-wrapper">r</div>
+            </div>
+            <div className="review-button-wrapper">
+                <button className="review-add" onClick={navigatePublish}>Add a Review</button>                
+            </div>
+            <div className="review-copyright-bar">
+                <p className="review-copyright-info">&copy; Jaren Worme 2024</p>
             </div>
         </div>
     );
